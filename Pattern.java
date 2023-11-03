b0fd61d4962ac6ca1ca2dc831ab7b7d06b9dfeb4
DPaul-11 <125825050+DPaul-11@users.noreply.github.com>
2023-11-03 23:57:03 +0530

    Add files via upload

---
+++ Pattern.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Pattern {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=0;j<i;j++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for(int i=n-1;i>=1;i--)
+        {
+            for(int j=i;j>=1;j--)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
