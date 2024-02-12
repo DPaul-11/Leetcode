88664c2b5b966bd398315058a5816f930ab61a40
DPaul-11 <125825050+DPaul-11@users.noreply.github.com>
2024-02-12 21:45:07 +0530

    Leetcode Solution

---
+++ Ransomenote_383.java
@@ -0,0 +1,33 @@
+package Leetcode;
+import java.util.*;
+
+public class Ransomenote_383 {
+    public static void main(String[] args) {
+        Boolean result=Ransomenote_383.canConstruct("aa","ab");
+        System.out.println(result);
+    }
+    public static boolean canConstruct(String ransomNote, String magazine) {
+        HashMap<Character, Integer> map=new HashMap<>();
+        for(int i=0;i<magazine.length();i++)
+        {
+            char c=magazine.charAt(i);
+            if(!map.containsKey(c))
+            {
+                map.put(c,1);
+            }else{
+                map.put(c,map.get(c)+1);
+            }
+        }
+        for(int i=0;i<ransomNote.length();i++)
+        {
+            char c=ransomNote.charAt(i);
+            if(map.containsKey(c) && map.get(c)>0)
+            {
+                map.put(c,map.get(c)-1);
+            }else{
+                return false;
+            }
+        }
+        return true;
+    }
+}
