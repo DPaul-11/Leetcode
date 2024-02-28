c51a9b55902b970f290c86aaf9337dbd82346113
Debobrata Paull <125825050+DPaul-11@users.noreply.github.com>
2024-02-28 21:23:52 +0530

    Add files via upload

---
+++ Find_bottom_left_tree_value_513.java
@@ -0,0 +1,39 @@
+package Leetcode;
+import java.util.*;
+
+public class Find_bottom_left_tree_value_513 {
+    public static void main(String[] args) {
+
+    }
+    public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+    public int findBottomLeftValue(TreeNode root) {
+        int last = 0;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+
+        while (!q.isEmpty()) {
+            int count = q.size();
+            for (int i = 0; i < count; i++) {
+                TreeNode curr = q.poll();
+                if (i == 0)
+                    last = curr.val;
+                if (curr.left != null)
+                    q.add(curr.left);
+                if (curr.right != null)
+                    q.add(curr.right);
+            }
+        }
+        return last;
+    }
+}
