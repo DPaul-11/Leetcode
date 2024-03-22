75647a6fb92c60c96710e5fcddff995e8001845f
Debobrata Paull <125825050+DPaul-11@users.noreply.github.com>
2024-03-22 20:49:29 +0530

    Leetcode solution

---
+++ Palindrome_linked_list_234.java
@@ -0,0 +1,46 @@
+package Leetcode;
+import java.util.*;
+
+public class Palindrome_linked_list_234 {
+    public static void main(String[] args) {
+        
+    }
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+    public boolean isPalindrome(ListNode head) {
+        ListNode slow=head;
+        ListNode fast=head;
+        ListNode prev=null;
+        if(head==null || head.next==null)
+        {
+            return true;
+        }
+        while(fast!=null && fast.next!=null)
+        {
+            fast=fast.next.next;
+            ListNode temp=slow.next;
+            slow.next=prev;
+            prev=slow;
+            slow=temp;
+        }
+        if(fast!=null)
+        {
+            slow=slow.next;
+        }
+        while(prev!=null && slow!=null)
+        {
+            if(prev.val!=slow.val)
+            {
+                return false;
+            }
+            prev=prev.next;
+            slow=slow.next;
+        }
+        return true;
+    }
+}
