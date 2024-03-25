bd2d6b5de2724934b213b716183b190eb43f1d15
Debobrata Paull <125825050+DPaul-11@users.noreply.github.com>
2024-03-25 12:34:25 +0530

    Leetcode solution

---
+++ Find_all_duplicates_in_array_442.java
@@ -0,0 +1,23 @@
+package Leetcode;
+import java.util.*;
+
+public class Find_all_duplicates_in_array_442 {
+    public static void main(String[] args) {
+        int nums[]={4,3,2,7,8,2,3,1};
+        Find_all_duplicates_in_array_442 ob=new Find_all_duplicates_in_array_442();
+        System.out.println(ob.findDuplicates(nums));
+    }
+    public List<Integer> findDuplicates(int[] nums) {
+        List<Integer> ans=new ArrayList<>();
+        for(int i=0;i<nums.length;i++)
+        {
+            int x = Math.abs(nums[i]);
+            if (nums[x - 1] < 0) {
+                ans.add(x);
+            }
+            nums[x - 1] *= -1;
+        }
+        return ans;
+    }
+}
+
