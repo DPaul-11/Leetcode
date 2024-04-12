5bfbcd8a86604de0535d719abce4780e67c2cf3f
Debobrata Paull <125825050+DPaul-11@users.noreply.github.com>
2024-04-12 15:25:18 +0530

    Leetcode solution

---
+++ Trapping_rainwater_42.java
@@ -0,0 +1,30 @@
+package Leetcode;
+
+public class Trapping_rainwater_42 {
+    public static void main(String[] args) {
+        int[] height={0,1,0,2,1,0,1,3,2,1,2,1};
+        Trapping_rainwater_42 ob=new Trapping_rainwater_42();
+        System.out.println(ob.trap(height));
+    }
+    public int trap(int[] height) {
+        int n=height.length;
+        int ans=0;
+        int[] left=new int[n];
+        int[] right=new int[n];
+        left[0]=height[0];
+        for(int i=1;i<n;i++)
+        {
+            left[i]=Math.max(left[i-1],height[i]);
+        }
+        right[n-1]=height[n-1];
+        for(int i=n-2;i>=0;i--)
+        {
+            right[i]=Math.max(right[i+1],height[i]);
+        }
+        for(int i=0;i<n;i++)
+        {
+            ans+=Math.min(left[i], right[i])-height[i];
+        }
+        return ans;
+    }
+}
